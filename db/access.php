f1e505f59613842f348948d5e24e3684d837e797
Dietmar Angerer <dangerer@gtn-solutions.com>
2013-01-29 15:51:25 +0100

    new capability addinstance

--- db/access.php
+++ db/access.php
@@ -96,7 +96,24 @@ $capabilities = array(
             'teacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         )
-    )
+    ), 
+    'block/exaport:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+        'user' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+    'block/exaport:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
 );
 
 ?>

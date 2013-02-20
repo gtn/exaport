bb6b59c962d04614d788d9f7b849029006a23dfe
Florian Jungwirth <jungwirth.florian@gmail.com>
2013-02-20 11:08:29 +0100

    Benutzer bei "Gesamtansicht" zu einem ePortfolio in einem Kurs weiterleiten, in dem er eingeschrieben ist. Sonst zur Startseite

--- epopalm.php
+++ epopalm.php
@@ -1,56 +1,45 @@
 <?php
- require_once dirname(__FILE__) . '/inc.php';
- require_once dirname(__FILE__) . '/lib/lib.php';
- //require_once dirname(__FILE__) . '/lib/moodlelib.php';
- require_once dirname(__FILE__) . '/lib/sharelib.php';
- global $DB,$USER,$COURSE,$CFG;
+require_once dirname(__FILE__) . '/inc.php';
+require_once dirname(__FILE__) . '/lib/lib.php';
+//require_once dirname(__FILE__) . '/lib/moodlelib.php';
+require_once dirname(__FILE__) . '/lib/sharelib.php';
+global $DB,$USER,$COURSE,$CFG;
 
-	$user=checkhash();
-	$gotologin=false;
-	if (!$user) $gotologin=true;
-	else{
-		if ($user->auth=='nologin' || $user->firstaccess==0 || $user->suspended!=0 || $user->deleted!=0){
-			$gotologin=true;
-		}else{
+$user=checkhash();
+$gotologin=false;
+if (!$user) $gotologin=true;
+else{
+	if ($user->auth=='nologin' || $user->firstaccess==0 || $user->suspended!=0 || $user->deleted!=0){
+		$gotologin=true;
+	}else{
 			
-			/*$sql="SELECT c.id FROM {course} c INNER JOIN {enrol} e on e.courseid=c.id INNER JOIN {user_enrolments} ue ON ue.enrolid=e.id";
-			$sql.=" WHERE c.visible=1 AND ue.userid=".$user->id;
-			$sql.=" ORDER BY ue.timemodified DESC LIMIT 0,1";
-			if ($rs = $DB->get_record_sql($sql)){*/
-
-				if ((!isloggedin())){
-					complete_user_login($user);
-				}
-				
-				//flo, hier einen kurs suchen, in dem exabis eportfolio installiert ist, und auf die view_items seite gehen
-				//wenn kein kurs, dann diesen redirect belassen
-				//wenn eportfolio nicht in einem kurs installiert ist, dorthin verlinken
-				
-				redirect($CFG->wwwroot);
-				
-				
-				
-				/*
-				$courses=enrol_get_my_courses();
-				if (!empty($courses) && is_array($courses)){
-					$first_key = key($courses);
-					redirect($CFG->wwwroot."/blocks/exaport/view.php?courseid=".$first_key);
-					
-				}else{
-					$gotologin=true;
-				}*/
-			
-			
+		if ((!isloggedin())){
+			complete_user_login($user);
 		}
+
+		//if user is enrolled to a course with exaport, redirect to it
+		//else redirect to moodle root
+		$mycourses=enrol_get_my_courses();
+		foreach($mycourses as $mycourse) {
+			$mycoursecontext = get_context_instance(CONTEXT_COURSE, $mycourse->id);
+			if($DB->record_exists('block_instances', array('blockname'=>'exaport', 'parentcontextid'=>$mycoursecontext->id))) {
+				redirect($CFG->wwwroot.'/blocks/exaport/view_items.php?courseid='.$mycourse->id);
+			}
+		}
+		unset($mycourses);
+		unset($mycoursecontext);
+		
+		redirect($CFG->wwwroot);
 	}
-	if ($gotologin==true){
-		redirect(get_login_url());
-	}
-	
+}
+if ($gotologin==true){
+	redirect(get_login_url());
+}
+
 function checkhash(){
 	global $DB;global $USER;
 	$userhash = optional_param('key', 0, PARAM_ALPHANUM);
-	$sql="SELECT u.* FROM {user} u INNER JOIN {block_exaportuser} eu ON eu.user_id=u.id WHERE eu.user_hash_long='".$userhash."'";					 
+	$sql="SELECT u.* FROM {user} u INNER JOIN {block_exaportuser} eu ON eu.user_id=u.id WHERE eu.user_hash_long='".$userhash."'";
 	if (!$user=$DB->get_record_sql($sql)){
 		return false;
 	}else{

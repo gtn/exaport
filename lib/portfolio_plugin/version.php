944d22fca6fc3aee9189b6a076dc2fd948523af2
Dietmar Angerer <dangerer@gtn-solutions.com>
2013-05-02 10:19:46 +0200

    version increase

--- lib/portfolio_plugin/version.php
+++ lib/portfolio_plugin/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012122000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013042301;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'portfolio_exaport'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;

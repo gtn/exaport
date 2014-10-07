be60dcd9e6e9ddb457906f2014a8a80f4a088f58
Dietmar Angerer <dangerer@gtn-solutions.com>
2014-10-07 12:02:22 +0200

    increase version number

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014092600;
+$plugin->version = 2014100700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2014041100;
 $plugin->release = 'v2.7-r1';

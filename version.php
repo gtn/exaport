094bf6cfe5a44e59ef65b78f3a6cf129ded1dfb5
Dietmar Angerer <uab.katzinger@gmail.com>
2015-12-11 15:42:04 +0100

    increase version number

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015110900;
+$plugin->version = 2015121100;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2014041100;
 $plugin->release = 'v4.3';

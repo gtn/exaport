60d5902ffab23f7c61826276c479569d0a0e0824
dprieler <dprieler@gmail.com>
2016-07-12 17:29:27 +0200

    version

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exaport';
-$plugin->release = '4.4.2.release';
-$plugin->version   = 2016062700;
+$plugin->release = '4.4.3.experimental';
+$plugin->version   = 2016062701;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;

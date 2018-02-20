9c97b5558b2024f8e00593a1ac26b255e1e09e81
Dietmar Angerer <uab.katzinger@gmail.com>
2018-02-20 09:20:22 +0100

    increase version number

--- version.php
+++ version.php
@@ -21,6 +21,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exaport';
 $plugin->release = '4.6.2';
-$plugin->version   = 2017121101;
+$plugin->version   = 2018020700;
 $plugin->requires  = 2016120505;
 $plugin->maturity = MATURITY_STABLE;

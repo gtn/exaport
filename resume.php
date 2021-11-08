46512c3f319adb23bb589351725a2097243f2da4
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-11-08 12:12:53 +0100

    test hello world commit

--- resume.php
+++ resume.php
@@ -15,6 +15,11 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 // (c) 2016 GTN - Global Training Network GmbH <office@gtn-solutions.com>.
 
+
+
+echo "hello World";
+die;
+
 require_once(__DIR__.'/inc.php');
 require_once(__DIR__.'/lib/resumelib.php');
 

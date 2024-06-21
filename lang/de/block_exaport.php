daeb9c279f2f3384240551655ff36ee3b9b447a6
Dietmar Angerer <uab.katzinger@gmail.com>
2024-06-21 08:07:11 +0200

    wording

--- lang/de/block_exaport.php
+++ lang/de/block_exaport.php
@@ -823,7 +823,7 @@ $string['create_view_content_help_text'] = '<ul>
 			<strong>Denke daran: Dein e-Portfolio erzählt der Welt von dir und deinen Fähigkeiten. Mache es klar, prägnant und ansprechend!</strong>';
 $string['moreinfolink'] = 'Mehr Informationen anzeigen ...';
 $string['badges_help'] = 'Der Block mit dir verliehenen Abzeichen<br><a href=\'{$a}\' target=\'_blank\' class=\'external-link\'>Deine Abzeichen anzeigen</a>';
-$string['block_exaport_used_layout'] = 'Used layout';
-$string['block_exaport_used_layout_body'] = 'Change the layout if you have some appearance problems. Right now it is used only for artefact list.';
-$string['block_exaport_used_layout_moodle_bootstrap'] = 'Cards layout';
-$string['block_exaport_used_layout_clean_old'] = 'Tiles layout (Old - ended of support from v4.6.7)';
+$string['block_exaport_used_layout'] = 'Verwendetes Layout';
+$string['block_exaport_used_layout_body'] = 'Ändern Sie das Layout, falls es Darstellungsprobleme gibt. Momentan wird es nur für die Artefaktliste verwendet.';
+$string['block_exaport_used_layout_moodle_bootstrap'] = 'Karten-Layout';
+$string['block_exaport_used_layout_clean_old'] = 'Kachel-Layout (Alt - Unterstützung endet mit Version 4.6.7)';

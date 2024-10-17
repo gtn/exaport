1ab7d424066bed76dc5e0d40b1723b9e7876e46b
Dietmar Angerer <uab.katzinger@gmail.com>
2024-10-17 15:21:51 +0200

    missing file

---
+++ portfolio_plugin/exaport/lang/en/portfolio_exaport.php
@@ -0,0 +1,19 @@
+<?php
+// This file is part of Exabis Eportfolio (extension for Moodle)
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// (c) 2016 GTN - Global Training Network GmbH <office@gtn-solutions.com>.
+
+require(substr(__FILE__, 0, strpos(__FILE__, 'exaport')).'/../blocks/exaport/lib/portfolio_plugin'.
+        substr(__FILE__, strpos(__FILE__, 'exaport') + 7));

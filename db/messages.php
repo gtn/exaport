e39c799f344e324502b828494c534478be1df55a
Dietmar Angerer <uab.katzinger@gmail.com>
2024-12-13 10:48:25 +0100

    deprecated fix moodle 4.5

--- db/messages.php
+++ db/messages.php
@@ -20,8 +20,8 @@ defined('MOODLE_INTERNAL') || die();
 $messageproviders = array(
     'sharing' => array(
         'defaults' => array(
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
-            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDOFF,
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
+            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
         ),
     ),
 );

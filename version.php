ac2f2ba887f5fd6d922f7236e56783580b3c4cea
Dietmar Angerer <uab.katzinger@gmail.com>
2025-11-27 12:21:09 +0100

    bump version for experimental

--- version.php
+++ version.php
@@ -20,5 +20,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_exaport';
 $plugin->release = '4.6.7';
 $plugin->version = 2025110100;
-$plugin->requires = 2021051700; // moodle 3.11
+$plugin->requires = 2021051701; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;

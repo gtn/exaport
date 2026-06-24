496c1a2c57dea19dae5875fd6105147edf8ed42b
rwolf <rwolfmayr@gtn-solutions.com>
2026-06-24 14:12:35 +0200

    version

--- version.php
+++ version.php
@@ -19,6 +19,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exaport';
 $plugin->release = '5.1';
-$plugin->version = 2026060300;
+$plugin->version = 2026060400;
 $plugin->requires = 2021051700; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;
